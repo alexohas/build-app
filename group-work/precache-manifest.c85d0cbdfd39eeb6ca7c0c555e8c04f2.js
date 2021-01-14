3fb63e673fb1868eb8f0c26ff123e7249267f464
Ahman Emmanuel <ahmanemmanuel@gmail.com>
2021-01-14 08:12:32 +0100

    ready

---
+++ group-work/precache-manifest.c85d0cbdfd39eeb6ca7c0c555e8c04f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19839743dbf8612255db",
+    "url": "/css/app.b51c4f7f.css"
+  },
+  {
+    "revision": "77ad6e81bb9ea635a09c",
+    "url": "/css/chunk-vendors.c5b0137c.css"
+  },
+  {
+    "revision": "0cd19c6592eb71be8482383106729fed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19839743dbf8612255db",
+    "url": "/js/app.cc1d0dc8.js"
+  },
+  {
+    "revision": "77ad6e81bb9ea635a09c",
+    "url": "/js/chunk-vendors.204eaca9.js"
+  },
+  {
+    "revision": "0de5c782ab348799d8b9957a9b64b276",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
